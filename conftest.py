c3bebce4a262fe22d77367b892e554ffe3d5ecc2
prakashrajv2017ece-code <prakashraj.v.2017.ece@ritchennai.edu.in>
2026-09-23 18:32:37 +0530

    Add GitHub Actions for Playwright tests

--- conftest.py
+++ conftest.py
@@ -1,14 +1,32 @@
+# import pytest
+# from playwright.sync_api import sync_playwright
+
+# @pytest.fixture(scope="function")
+# def page():
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=False)
+#         context = browser.new_context()
+#         context.tracing.start(screenshots=True, snapshots=True)
+#         page = context.new_page()
+#         yield page
+#         context.tracing.stop(path="tracing.zip")
+#         browser.close()
+
+import os
 import pytest
 from playwright.sync_api import sync_playwright
+
 
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        headless = os.getenv("CI", "false").lower() == "true"
+
+        browser = p.chromium.launch(headless=headless)
+
         context = browser.new_context()
-        context.tracing.start(screenshots=True, snapshots=True)
         page = context.new_page()
+
         yield page
-        context.tracing.stop(path="tracing.zip")
-        browser.close()
-    
+
+        browser.close()    
